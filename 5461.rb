e9ffa9cc53f244cdf6d793765e28567d74a822d1
khalid benjelloun <khalidmailing@gmail.com>
2021-08-12 09:12:41 +0100

    Added ruby file

---
+++ 5461.rb
@@ -0,0 +1,19 @@
+def is_divisible(start)
+  sum = 0
+  array = []
+  exp = ""
+  7.times do
+    array << "4^" + start.to_s
+    sum += 4 ** start
+    start += 1
+  end
+  exp = array.join(" \+ ")
+  print exp + " = " + sum.to_s
+end
+
+start = 0
+100.times do
+  print is_divisible(start)
+  print "\n"
+  start += 1
+end
